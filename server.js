b3c87bf908b8fb7284d43a0881f79f1a7519cd35
Parinita Salagala <parinitasalagala@gmail.com>
2025-10-23 00:55:02 +0530

    can view only my posts

--- server.js
+++ server.js
@@ -137,6 +137,16 @@ app.post('/new',async(req,res)=>{
 
 });
 
+app.get('/posts',async (req,res)=>{
+    const userId=req.session.userId;
+    if(!userId) return res.redirect('/login');
+
+  const users=(await pool.query("SELECT * FROM users WHERE user_id=$1", [userId])).rows[0];
+   const posts=(await pool.query("SELECT * FROM posts where user_id=$1",[userId])).rows;
+   res.render("posts",{users,posts});
+
+})
+
 app.get('/posts/:post_id',async(req,res)=>{
   const post_id=req.params.post_id;
   const userId=req.session.userId;
